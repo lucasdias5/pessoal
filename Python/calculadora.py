61cec8e98210caed4e7f70578d380655a6514364
lucasdias5 <diaslucas9940@gmail.com>
2026-03-22 17:30:10 -0300

    nome das variaveis, variaveis novas, organização

--- Python/calculadora.py
+++ Python/calculadora.py
@@ -1,25 +1,64 @@
 while True:
     import math
-    c = "CALCULADORA"
-    print("\n{:=^20}".format(c))
+
+    #titulo
+
+    print("\n{:=^20}".format("CALCULADORA"))
     
-    x = float(input("\ninforme o primeiro valor: "))
+    #entrada
+
+    primeiro_valor = float(input("\ninforme o primeiro valor: "))
     operacao = input("escolha a operação: ")
-    if operacao == "raiz quadrada":print("resultado:", math.sqrt(x))
-    if operacao == "raiz quadrada":continue
-    y = float(input("informe o segundo valor: "))
+    
+    #caso a operacao seja raiz quadrada
 
-    por1 = (x*y)/100
+    if operacao == "raiz quadrada":
+        print("resultado:", math.sqrt(primeiro_valor))
+        loop_raiz = str(input("Deseja continuar?: "))
+        if loop_raiz == "sim":
+            continue
+        elif loop_raiz == "nao":
+            break    
 
-    if operacao == "-":print("resultado: ", x - y)
-    elif operacao == "*":print("resultado: ", x * y)
-    elif operacao == "/":print("resultado:", x / y)
-    elif operacao == "+":print("resultado: ", x + y)
-    elif operacao == "potencia":print("resultado: ", x**y)
-    elif operacao == "%":print("resultado: ", (x*y)/100)
-    elif operacao == "+%":print("resultado: ", x + por1)
-    elif operacao == "-%":print("resultado: ", x - por1)
-    else:print("erro, operacao invalida")
+    segundo_valor = float(input("informe o segundo valor: "))
 
-    c = input("Deseja repetir?: ")
-    if c == "não":break
+    #variaveis das operacoes
+
+    soma = primeiro_valor + segundo_valor
+    subtracao = primeiro_valor - segundo_valor
+    multiplicacao = primeiro_valor * segundo_valor
+    divisao = primeiro_valor / segundo_valor
+    potencia = primeiro_valor ** segundo_valor
+    porcentagem = (primeiro_valor * segundo_valor) / 100
+    soma_porcentagem = primeiro_valor + ((primeiro_valor * segundo_valor) / 100)
+    subtracao_porcentagem = primeiro_valor - ((primeiro_valor * segundo_valor) / 100)
+
+    #processamento e saida
+
+    if operacao == "+":
+        print("resultado: ", soma)
+    elif operacao == "-":
+        print("resultado: ", subtracao)
+    elif operacao == "*":
+        print("resultado:", multiplicacao)
+    elif operacao == "/":
+        print("resultado: ", divisao)
+    elif operacao == "potencia":
+        print("resultado: ", potencia)
+    elif operacao == "%":
+        print("resultado: ", porcentagem)
+    elif operacao == "+%":
+        print("resultado: ", soma_porcentagem)
+    elif operacao == "-%":
+        print("resultado: ", subtracao_porcentagem)
+    else:
+        print("Erro: operacao invalida")
+
+    #loop
+
+    loop = input("Deseja repetir?: ")
+
+    if loop == "nao":
+        break
+    elif loop == "sim":
+        continue
