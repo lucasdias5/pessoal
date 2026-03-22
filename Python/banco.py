617ca2cbd5886c58c8e0c4faf02eb33768cf2eea
lucasdias5 <diaslucas9940@gmail.com>
2026-03-22 01:48:21 -0300

    corrigi a mensagem de erro na operação e deixei mais organizado

--- Python/banco.py
+++ Python/banco.py
@@ -13,20 +13,35 @@ while True:
     print("Aperte 2 para depositar")
 
     operacao = int(input("\nVoce deseja sacar ou depositar?: "))
-    valor = float(input("Qual valor você deseja sacar/depositar?: "))
 
-    #se der erro
+    if operacao == 1:
+        valor = float(input("Qual valor você deseja sacar?: "))
+    elif operacao == 2:
+        valor = float(input("Qual valor você deseja depositar: "))
+    else:
+        print("\nErro: operacao invalida")
+        print("\nAperte 1 para repetir")
+        print("Aperte 2 para sair")
+
+        loop_erro_valor = int(input("Voce deseja repetir?: "))
+        
+        if loop_erro_valor == 1:
+            continue
+        elif loop_erro_valor == 2:
+            break
+
+    #se o saldo for insuficiente
 
     if operacao == 1 and valor > saldo:
         print("\nErro: saldo insuficiente")
         print("\nAperte 1 para repetir")
         print("Aperte 2 para sair")
 
-        loop_erro = int(input("Voce deseja repetir?: \n"))
+        loop_erro_valor = int(input("Voce deseja repetir?: "))
         
-        if loop_erro == 1:
+        if loop_erro_valor == 1:
             continue
-        elif loop_erro == 2:
+        elif loop_erro_valor == 2:
             break
     
     #processamento e saída
@@ -35,19 +50,15 @@ while True:
         print("\nVoce sacou: ",valor, "\nSeu saldo em conta agora é: ", saldo - valor)
     elif operacao == 2:
         print("\nVoce depositou: ",valor, "\nSeu saldo em conta agora é: ", saldo + valor)
-    else:
-        print("\nOperacao invalida")
 
     #loop
-    
+
     print("\nAperte 1 para repetir")
     print("Aperte 2 para sair")
 
-    loop = int(input("Voce deseja repetir?: \n"))
+    loop = int(input("Voce deseja repetir?: "))
 
     if loop == 1:
         continue
     elif loop == 2:
         break
-
-        
