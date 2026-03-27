932fc3204c916686bd08419ae0ee73ce877ab642
lucasdias5 <diaslucas9940@gmail.com>
2026-03-26 21:37:57 -0300

    Teste de conteudo novo

---
+++ JavaScript/teste/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+
+let variavel1 = 1
+console.log(variavel1)
+variavel1 = 2
+console.log(variavel1)
+
+const variavel2 = 10
+console.log(variavel2)
+
+let nome = "Lucas"
+let sobrenome = "Dias"
+
+console.log(nome + " " + sobrenome)
